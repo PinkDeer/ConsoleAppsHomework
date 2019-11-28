2a960e929c5801ff8e7b46dcd46a9c45b42dd978
PinkDeer <andreyzinovets@gmail.com>
2019-11-28 19:07:20 +0300

    Add project files.

---
+++ ConsoleAppsHomework/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "First Console App";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Let`s to do homework!");
+            Console.ReadKey();
+        }
+    }
+}
