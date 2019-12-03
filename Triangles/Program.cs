fdba1186e2904693a1608197b349c1b01ecfeedd
PinkDeer <andreyzinovets@gmail.com>
2019-12-03 12:13:02 +0300

    Update Triangles

--- Triangles/Program.cs
+++ Triangles/Program.cs
@@ -3,89 +3,174 @@
 namespace Triangles
 {
 
-    class Program
+    /*    class Program
+        {
+            static void Main(string[] args)
+            {
+
+                // Использую вложенные массивы нарисовать в консоли треугольники
+
+                Console.Title = "Треугольники";
+
+                // Первый треугольник
+
+                Console.Write("Высота треугольника: ");
+                int height = int.Parse(Console.ReadLine());
+
+                uint count = 1;
+
+                for (int i = 1; i <= height; i++)
+                {
+                    for (int j = 1; j <= count; j++)
+                    {
+                        Console.Write("#");
+                    }
+                    Console.WriteLine();
+                    count += 1;
+                }
+
+                Console.WriteLine();
+
+                // Второй треугольник
+
+                count = 1;
+
+                for (int i = 0; i < height; i++)
+                {
+                    for (int j = height; j >= count; j--)
+                    {
+                        Console.Write("#");
+                    }
+                    Console.WriteLine();
+                    count += 1;
+                }
+
+                Console.WriteLine();
+
+                // Третий треугольник
+
+                count = 1;
+
+                for (int i = 1; i <= height; i++)
+                {
+                    for (int j = (height-1); j >= count; j--)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int k = 1; k <= count; k++)
+                    {
+                        Console.Write("#");
+                    }
+                    Console.WriteLine();
+                    count += 1;
+                }
+
+                Console.WriteLine();
+
+                // Четвёртый треугольник
+
+                count = 1;
+
+                for (int i = 1; i <= height; i++)
+                {
+                    for (int j = 2; j <= count; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int k = height; k >= count; k--)
+                    {
+                        Console.Write("#");
+                    }
+                    Console.WriteLine();
+                    count += 1;
+                }
+
+                Console.ReadLine();
+            }
+        }*/
+
+    // Вариант 2
+
+/*    class Program2
     {
         static void Main(string[] args)
         {
-
-            // Использую вложенные массивы нарисовать в консоли треугольники
-
-            Console.Title = "Треугольники";
+            Console.Write("Высота высоту треугольника: ");
+            int height = int.Parse(Console.ReadLine());
 
             // Первый треугольник
 
-            Console.Write("Высота треугольника: ");
-            int height = int.Parse(Console.ReadLine());
-
-            uint count = 1;
-
-            for (int i = 1; i <= height; i++)
+            for (int i = 0; i < height; i++)
             {
-                for (int j = 1; j <= count; j++)
+                for (int j = 0; j <= i; j++)
                 {
-                    Console.Write("#");
+                    Console.Write("*");
                 }
                 Console.WriteLine();
-                count += 1;
             }
 
-            Console.WriteLine();
+            Console.ReadLine();
 
             // Второй треугольник
 
-            count = 1;
-
             for (int i = 0; i < height; i++)
             {
-                for (int j = height; j >= count; j--)
+                for (int j = height; j > i; j--)
                 {
-                    Console.Write("#");
+                    Console.Write("*");
                 }
                 Console.WriteLine();
-                count += 1;
             }
 
-            Console.WriteLine();
+            Console.ReadLine();
 
             // Третий треугольник
 
-            count = 1;
-
-            for (int i = 1; i <= height; i++)
+            for (int i = 0; i < height; i++)
             {
-                for (int j = (height-1); j >= count; j--)
+                for (int j = height-1; j > i; j--)
                 {
                     Console.Write(" ");
                 }
-                for (int k = 1; k <= count; k++)
+                for (int j = 0; j <= i; j++)
                 {
-                    Console.Write("#");
+                    Console.Write("*");
                 }
                 Console.WriteLine();
-                count += 1;
             }
 
-            Console.WriteLine();
+            Console.ReadLine();
 
-            // Четвёртый треугольник
+            // Третий треугольник версия 2
 
-            count = 1;
-
-            for (int i = 1; i <= height; i++)
+      *//*      for (int i = 0; i < height; i++)
             {
-                for (int j = 2; j <= count; j++)
+                Console.SetCursorPosition(height - i, i + 1);
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+
+            }
+
+            Console.ReadLine();*//*
+
+            // Четввертый треугольник
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < i; j++)
                 {
                     Console.Write(" ");
                 }
-                for (int k = height; k >= count; k--)
+                for (int j = height; j > i; j--)
                 {
-                    Console.Write("#");
+                    Console.Write("*");
                 }
                 Console.WriteLine();
-                count += 1;
             }
 
             Console.ReadLine();
         }
-    }
+    }*/
 }
